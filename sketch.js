e23d3cb4819a8a0be71fe8ac0234f48d9a4af12a
Raghav-tech990 <72139112+Raghav-tech990@users.noreply.github.com>
2020-11-23 23:03:15 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -23,22 +23,26 @@ function setup() {
 	helicopterSprite=createSprite(width/2, 200, 10,10);
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6
-
+``
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
+
+	box1=new Box(width/2,650,200,20)
+    box2=new Box(300,610,20,100)
+    box3=new Box(500,610,20,100)
 
 
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , { isStatic:true});
 	World.add(world, packageBody);
 	
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
-
+ 
 
 	Engine.run(engine);
   
@@ -57,7 +61,8 @@ function draw() {
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
     // Look at the hints in the document and understand how to make the package body fall only on
-    
+	Matter.Body.setStatic(packageBody,false);
+ 
   }
 }
 
